2170dae55ade8c9c7a068151d52fdee477780c84
5z3wczykj4kub <5z3wczykj4kub@gmail.com>
2021-07-22 16:01:20 +0200

    :sunglasses:

---
+++ main.js
@@ -0,0 +1,41 @@
+const secondsHand = document.querySelector('#seconds-hand');
+const minutesHand = document.querySelector('#minutes-hand');
+const hoursHand = document.querySelector('#hours-hand');
+const currentTime = document.querySelector('#current-time');
+const currentDate = document.querySelector('#current-date');
+const toggleButton = document.querySelector('#toggle button');
+
+// initial date
+currentDate.textContent = `${new Date().toLocaleDateString()}`;
+
+// set clock
+setInterval(setClock, 1000);
+
+// check prefered color scheme
+const isPreferedColorSchemeDark = window.matchMedia(
+  '(prefers-color-scheme: dark)'
+).matches;
+if (!isPreferedColorSchemeDark) document.body.classList.add('light-mode');
+
+// toggle light mode
+toggleButton.addEventListener('click', () =>
+  document.body.classList.toggle('light-mode')
+);
+
+function setClock() {
+  const date = new Date();
+
+  const secondsHandDeg = (date.getSeconds() * 6) % 360;
+  const minutesHandDeg = (date.getMinutes() * 6) % 360;
+  const hoursHandDeg = (date.getHours() * 30) % 360;
+
+  secondsHand.style.transform = `rotate(${secondsHandDeg}deg)`;
+  minutesHand.style.transform = `rotate(${minutesHandDeg}deg)`;
+  hoursHand.style.transform = `rotate(${hoursHandDeg}deg)`;
+
+  currentTime.textContent = `${
+    date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
+  }:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}:${
+    date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
+  }`;
+}
